dd6dbae9f771b435cb0903c750493d6bbaf9db3f
Carol Chen <awdrg1210@gmail.com>
2020-03-09 12:22:23 +0800

    config improvement and ad env to config.py

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -5,22 +5,23 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from .common.connection import db
-from .config import Config
+from .config import app_config
 from .model.user import User as UserModel
 from .resource.hello import Helloworld
 from .resource.tweet import Tweet
 from .resource.user import User, UserList
 
+migrate = Migrate()
 jwt = JWT(None, UserModel.authenticate, UserModel.identity)
 
 
-def create_app():
+def create_app(config_name='development'):
 
     app = Flask(__name__)
     api = Api(app)
-    app.config.from_object(Config)
+    app.config.from_object(app_config[config_name])
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate.init_app(app, db)
     jwt.init_app(app)
 
     api.add_resource(Helloworld, '/')
--- restapi/config.py
+++ restapi/config.py
@@ -6,11 +6,33 @@ from datetime import timedelta
 
 class Config:
 
-    PROJ_ROOT = dirname(dirname(abspath(__file__)))
-    SQLITE_PATH = join(PROJ_ROOT, 'user.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'sqlite:///{SQLITE_PATH}')  # noqa: E501
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'super-secret'
     JWT_EXPIRATION_DELTA = timedelta(seconds=300)
     JWT_AUTH_URL_RULE = '/auth/login'
-    JWT_AUTH_HEADER_PREFIX = 'FLASK'
+    JWT_AUTH_HEADER_PREFIX = os.environ.get('JWT_AUTH_HEADER_PREFIX', 'FLASK')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class TestingConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class DevelopmentConfig(Config):
+
+    DEBUG = True
+    PROJ_ROOT = dirname(dirname(abspath(__file__)))
+    SQLITE_PATH = join(PROJ_ROOT, 'user.db')
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{SQLITE_PATH}'
+
+
+class ProductionConfig(Config):
+    pass
+
+
+app_config = {
+    'testing': TestingConfig,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
