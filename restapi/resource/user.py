de2f6a6fd38ebadebe6cf7266f5438b3496fcc99
Carol Chen <awdrg1210@gmail.com>
2020-03-09 13:51:42 +0800

    modify resource/user.py

--- restapi/resource/user.py
+++ restapi/resource/user.py
@@ -2,19 +2,11 @@ from flask_jwt import jwt_required
 from flask_restful import Resource
 
 from ..common import utils
-# from ..common.connection import db
 from ..model.user import User as UserModel
 
 
 class User(Resource):
 
-    # parser = reqparse.RequestParser()
-    # parser.add_argument(
-    #     'password',
-    #     type=utils.min_length_str(5),
-    #     required=True,
-    #     help='{error_msg}'
-    # )
     def __init__(self, *args, **kwargs):
         """Init."""
         super().__init__(*args, **kwargs)
@@ -22,9 +14,6 @@ class User(Resource):
 
     def get(self, username):
         """Get user detail information."""
-        # user = db.session.query(UserModel).filter(
-        #     UserModel.username == username
-        # ).first()
         user = UserModel.get_by_username(username)
         if user:
             return user.as_dict()
@@ -33,9 +22,6 @@ class User(Resource):
     def post(self, username):
         """Create a user."""
         data = self.parser.parse_args()
-        # user = db.session.query(UserModel).filter(
-        #     UserModel.username == username
-        # ).first()
         user = UserModel.get_by_username(username)
         if user:
             return {'message': 'User already exist.'}
@@ -44,8 +30,6 @@ class User(Resource):
             email=data['email']
         )
         user.set_password(data['password'])
-        # db.session.add(user)
-        # db.session.commit()
         user.add()
         return {
             'message': 'Insert user success.',
@@ -54,35 +38,26 @@ class User(Resource):
 
     def delete(self, username):
         """Delete user."""
-        # user = db.session.query(UserModel).filter(
-        #     UserModel.username == username
-        # ).first()
         user = UserModel.get_by_username(username)
         if user:
-            # db.session.delete(user)
-            # db.session.commit()
             user.delete()
             return {'message': 'User deleted.'}
         else:
-            return {'message': 'User not found.'}, 204
+            return {'message': 'User not found.'}, 404
 
     def put(self, username):
         """Update user."""
-        # user = db.session.query(UserModel).filter(
-        #     UserModel.username == username
-        # ).first()
         user = UserModel.get_by_username(username)
         if user:
             data = self.parser.parse_args()
             user.password_hash = data['password']
-            # db.session.commit()
             user.update()
             return {
                 'message': 'Update user success.',
                 'user': user.as_dict()
             }
         else:
-            return {'message': 'User not found.'}, 204
+            return {'message': 'User not found.'}, 404
 
 
 class UserList(Resource):
@@ -90,6 +65,5 @@ class UserList(Resource):
     @jwt_required()
     def get(self):
         """Get user list."""
-        # users = db.session.query(UserModel).all()
         users = UserModel.get_all_user()
         return [user.as_dict() for user in users]
